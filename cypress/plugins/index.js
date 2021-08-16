247808cafa08633fe804be5eb55bca15570d304b
valandi <valandi.allulis@gmail.com>
2021-08-16 14:43:57 -0400

    Commmitting Demo

---
+++ cypress/plugins/index.js
@@ -0,0 +1,8 @@
+const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
+
+module.exports = on => {
+  on('file:preprocessor', cypressTypeScriptPreprocessor)
+}
+
+
+require('@applitools/eyes-cypress')(module);
